eaba5e69633eabb1723772ed8fdffb10a6c6bfc7
Anggito Wicaksono <anggitov@gmail.com>
2025-05-25 00:21:47 +0700

    feat: frontend - login page check access token

---
+++ apps/frontend-repo/app/LoginForm.tsx
@@ -0,0 +1,96 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { Box, Button, Container, Typography, Paper, Grid } from "@mui/material";
+import { Google } from "@mui/icons-material";
+import { auth } from "../firebase/firebaseClient";
+
+const LoginForm = () => {
+  const provider = new GoogleAuthProvider();
+  const router = useRouter();
+
+  const loginWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, provider);
+      const user = auth.currentUser;
+      const token = await user?.getIdToken();
+
+      await fetch("/api/set-cookie", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ token }),
+      });
+
+      await fetch("http://localhost:3001/api/update-user-data", {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          uid: user?.uid,
+          name: user?.displayName,
+          email: user?.email,
+          photoURL: user?.photoURL,
+        }),
+      });
+
+      router.push("/home");
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+    }
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: "100vh" }}
+      >
+        <Grid width="100%" size={{ xs: 6 }}>
+          <Paper elevation={3} sx={{ padding: 4, borderRadius: 3 }}>
+            <Typography
+              variant="h5"
+              fontWeight="bold"
+              textAlign="center"
+              gutterBottom
+            >
+              Welcome Back
+            </Typography>
+            <Typography
+              variant="body1"
+              textAlign="center"
+              color="text.secondary"
+              mb={3}
+            >
+              Sign in to continue
+            </Typography>
+
+            <Box display="flex" flexDirection="column" gap={2}>
+              <Button
+                variant="outlined"
+                startIcon={<Google />}
+                size="large"
+                sx={{
+                  textTransform: "none",
+                  borderRadius: 2,
+                  fontWeight: 500,
+                }}
+                fullWidth
+                onClick={loginWithGoogle}
+              >
+                Continue with Google
+              </Button>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default LoginForm;
--- apps/frontend-repo/app/page.tsx
+++ apps/frontend-repo/app/page.tsx
@@ -1,94 +1,13 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { Box, Button, Container, Typography, Paper, Grid } from "@mui/material";
-import { Google } from "@mui/icons-material";
-import { auth } from "../firebase/firebaseClient";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import LoginForm from "./LoginForm";
 
-export default function Login() {
-  const provider = new GoogleAuthProvider();
-  const router = useRouter();
+export default async function Login() {
+  const token = (await cookies()).get("idToken");
 
-  const loginWithGoogle = async () => {
-    try {
-      await signInWithPopup(auth, provider);
-      const user = auth.currentUser;
-      const token = await user?.getIdToken();
+  if (token) {
+    redirect("/home");
+  }
 
-      await fetch("/api/set-cookie", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ token }),
-      });
-
-      await fetch("http://localhost:3001/api/update-user-data", {
-        method: "POST",
-        headers: {
-          Authorization: "Bearer " + token,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          uid: user?.uid,
-          name: user?.displayName,
-          email: user?.email,
-          photoURL: user?.photoURL,
-        }),
-      });
-
-      router.push("/home");
-    } catch (error) {
-      console.error("Error signing in with Google:", error);
-    }
-  };
-
-  return (
-    <Container maxWidth="sm">
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        sx={{ minHeight: "100vh" }}
-      >
-        <Grid width="100%" size={{ xs: 6 }}>
-          <Paper elevation={3} sx={{ padding: 4, borderRadius: 3 }}>
-            <Typography
-              variant="h5"
-              fontWeight="bold"
-              textAlign="center"
-              gutterBottom
-            >
-              Welcome Back
-            </Typography>
-            <Typography
-              variant="body1"
-              textAlign="center"
-              color="text.secondary"
-              mb={3}
-            >
-              Sign in to continue
-            </Typography>
-
-            <Box display="flex" flexDirection="column" gap={2}>
-              <Button
-                variant="outlined"
-                startIcon={<Google />}
-                size="large"
-                sx={{
-                  textTransform: "none",
-                  borderRadius: 2,
-                  fontWeight: 500,
-                }}
-                fullWidth
-                onClick={loginWithGoogle}
-              >
-                Continue with Google
-              </Button>
-            </Box>
-          </Paper>
-        </Grid>
-      </Grid>
-    </Container>
-  );
+  return <LoginForm />;
 }
